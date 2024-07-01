e3575c46b6501301a26bacce659d8ed8b31c688d
KeesGeerligs <166137421+KeesGeerligs@users.noreply.github.com>
2024-07-01 12:35:19 +0200

    1

--- vllm/test.py
+++
@@ -1,6 +0,0 @@
-from vllm import LLM
-
-prompts = ["What colour is the sky?"]  # Sample prompts.
-llm = LLM(model="meta-llama/Meta-Llama-3-8B-Instruct")  # Create an LLM.
-outputs = llm.generate(prompts)
-print(outputs)
