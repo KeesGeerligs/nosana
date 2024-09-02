81a1175f5a66cc394dfde63de869e3b33011c852
KeesGeerligs <166137421+KeesGeerligs@users.noreply.github.com>
2024-09-02 14:04:21 +0200

    1

--- bench_LMDeploy_llama31_8b_4x/run_benchmark_llm_bench.sh
+++ bench_LMDeploy_llama31_8b_4x/run_benchmark_llm_bench.sh
@@ -1,10 +1,3 @@
-#!/bin/bash
-echo "Current directory: $(pwd)"
-
-# List all files in the current directory
-echo "Files in the current directory:"
-ls -la
-
 # Start the lmdeploy service with provided arguments
 echo "Starting lmdeploy service..."
 lmdeploy serve api_server ../../root/snapshots/db1f81ad4b8c7e39777509fac66c652eb0a52f91 --model-name llama3.1 --model-format awq &
